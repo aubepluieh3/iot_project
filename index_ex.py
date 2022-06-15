1860e07d47979ef5897a01ff79b508ff4ead57a2
SOO <aubepluieh3@naver.com>
2022-06-16 00:54:58 +0900

    function

--- index_ex.py
+++ index_ex.py
@@ -12,6 +12,7 @@ import datetime
 from flask import Flask, request
 from flask import render_template
 import threading
+import os
 app = Flask(__name__)
 GPIO.setmode(GPIO.BOARD)                    #BOARD는 커넥터 pin번호 사용
 #led 핀 번호
@@ -24,6 +25,7 @@ GPIO.setup(red_pin, GPIO.OUT,initial=GPIO.LOW)
 GPIO.setup(music_pin, GPIO.OUT)
 
 
+
 # PWM 인스턴스 p를 만들고  GPIO 18번을 PWM 핀으로 설정, 주파수  = 100Hz
 p = GPIO.PWM(music_pin, 100)  
 c=262 #도
@@ -34,13 +36,12 @@ fs=370
 g=392
 a=440
 b=493
-c2=554
-d2=587
-e2=659
+
 
 # 4옥타브 도~시 , 5옥타브 도의 주파수 
 Frq1 = [c,d,e] #전원 부팅음
 Frq2 = [a,e ] #전원 꺼지는 소리
+#작은별 
 Frq3 = [c,c,g,g,a,a,g]
 Frq4 = [f,f,e,e,d,d,c]
 Frq5 = [g,g,f,f,e,e,d]
@@ -67,8 +68,10 @@ spi.max_speed_hz = 100000
 
 #전원 led
 power_led = False
+#전원 music
+power_music = False
 #음악
-music = False
+sound = False
 
 # 0 ~ 7 까지 8개의 채널에서 SPI 데이터를 읽어옵니다.
 def readadc(adcnum):
@@ -88,92 +91,100 @@ def home():
 
 
 
-
-
+#음악 재생
 @app.route("/music/on")                       
 def music_on():
+    global sound
     try:
-        while music == True:
-            p.start(10)  # PWM 시작 , 듀티사이클 10 (충분)
-            for fr in Frq3:
-                p.ChangeFrequency(fr)    #주파수를 fr로 변경
-                time.sleep(speed1)       #speed 초만큼 딜레이 (0.5s) 
-            time.sleep(speed2) 
+        if power_music == True:
+            sound = False
+            while sound ==False:
+                p.start(10)  # PWM 시작 , 듀티사이클 10 (충분)
+                for fr in Frq3:
+                    p.ChangeFrequency(fr)    #주파수를 fr로 변경
+                    time.sleep(speed1)       #speed 초만큼 딜레이 (0.5s) 
+                time.sleep(speed2) 
 
-            for fr in Frq4:
-                p.ChangeFrequency(fr)    #주파수를 fr로 변경
-                time.sleep(speed1)       #speed 초만큼 딜레이 (0.5s)
-            time.sleep(speed2)
+                for fr in Frq4:
+                    p.ChangeFrequency(fr)    #주파수를 fr로 변경
+                    time.sleep(speed1)       #speed 초만큼 딜레이 (0.5s)
+                time.sleep(speed2)
 
-            for fr in Frq5:
-                p.ChangeFrequency(fr)    #주파수를 fr로 변경
-                time.sleep(speed1)
-            time.sleep(speed2)           #speed 초만큼 딜레이 (0.8s) 
+                for fr in Frq5:
+                    p.ChangeFrequency(fr)    #주파수를 fr로 변경
+                    time.sleep(speed1)
+                time.sleep(speed2)           #speed 초만큼 딜레이 (0.8s) 
 
-            for fr in Frq5:
-                p.ChangeFrequency(fr)    #주파수를 fr로 변경
-                time.sleep(speed1)       #speed 초만큼 딜레이         
-            time.sleep(speed2)     
-                # GPIO 설정 초기화 
+                for fr in Frq5:
+                    p.ChangeFrequency(fr)    #주파수를 fr로 변경
+                    time.sleep(speed1)       #speed 초만큼 딜레이         
+                time.sleep(speed2)     
+                    # GPIO 설정 초기화 
 
-            for fr in Frq3:
-                p.ChangeFrequency(fr)    #주파수를 fr로 변경
-                time.sleep(speed1)       #speed 초만큼 딜레이 (0.5s) 
-            time.sleep(speed2) 
+                for fr in Frq3:
+                    p.ChangeFrequency(fr)    #주파수를 fr로 변경
+                    time.sleep(speed1)       #speed 초만큼 딜레이 (0.5s) 
+                time.sleep(speed2) 
 
-            for fr in Frq4:
-                p.ChangeFrequency(fr)    #주파수를 fr로 변경
-                time.sleep(speed1)       #speed 초만큼 딜레이 (0.5s)
-            time.sleep(speed2)
-
-            return "ok"                         # 함수가 'ok'문자열을 반환함
+                for fr in Frq4:
+                    p.ChangeFrequency(fr)    #주파수를 fr로 변경
+                    time.sleep(speed1)       #speed 초만큼 딜레이 (0.5s)
+                time.sleep(speed2)
+                return "ok"                         # 함수가 'ok'문자열을 반환함
     except :
         return "fail"
 
+#음악 정지
 @app.route("/music/off")         
 def music_off():
+    global sound
     try:
+        sound = True
         p.stop()
         return "ok"
     except:
         return "fail"
 
+  
 
+#블랙박스 작동
 def blackbox_start():
     while power_led ==True:
         with picamera.PiCamera() as camera:
             camera.resolution = (640,480)
             camera.start_preview()
             now= datetime.datetime.now()
-            filename=now.strftime('%Y-%m-%d %H:%M:%S')
+            filename=now.strftime('%Y-%m-%d %H:%M:%S') #현재날짜시각
             camera.start_recording(output=filename+'.h264')
-            camera.wait_recording(10)
+            camera.wait_recording(60) #60초
             camera.stop_recording()
             camera.stop_preview()          
 
+#속도
+@app.route("/speed")
 def speed_start():
-    while True:
+    while power_led ==True: #전원이 켜져야만 실행됨
         vrx_pos = readadc(vrx_channel)
         vry_pos = readadc(vry_channel)
         sw_val = readadc(sw_channel)
         global speed
         if(vrx_pos == 0 and sw_val == 1023 and speed<100): # SPEED UP , 최대속도 100으로 설정
             speed = speed+3     
-            print(speed) 
             time.sleep(1)   # 1초동안 대기상태
+            return str(speed) 
         elif(vrx_pos == 518 and vry_pos == 504 and sw_val == 1023): #MAINTAIN
-            print(speed)
             time.sleep(1)   # 1초동안 대기상태
+            return str(speed) 
 
         elif(vrx_pos == 1023 and sw_val == 1023 and speed>0): #SPEED DOWN 
             speed = speed-3
-            print(speed)      
             time.sleep(1)   # 1초동안 대기상태
-
+            return str(speed) 
+    return "speed"
 
 
 def button():
-    global power_led
+    global power_led, power_music
     if(power_led == False): #LED가 안 들어와 있다면
         GPIO.output(red_pin,1) #전원 불 켜기
 
@@ -184,13 +195,13 @@ def button():
         time.sleep(speed2) 
         p.stop()      # GPIO 설정 초기화 
         power_led = True
-        music = True
+        power_music = True
         #블랙박스 작동
         t2 = threading.Thread(target=blackbox_start)   # Thread t2 생성
         #speed 작동
-        t3 = threading.Thread(target=speed_start) # Thread t3 생성
+        #t3 = threading.Thread(target=speed_start) # Thread t3 생성
         t2.start()
-        t3.start()
+        #t3.start()
 
     else:
         GPIO.output(red_pin,0)
@@ -201,14 +212,13 @@ def button():
         time.sleep(speed2) 
         p.stop()      # GPIO 설정 초기화 
         power_led= False
-        music=False
+        power_music=False
         exit()
                                  
 
 
 def joystick():
     while True:
-
         # 스위치 입력
         sw_val = readadc(sw_channel)
         if(sw_val==0): #버튼을 눌렀을 때 (전원 on/off )
